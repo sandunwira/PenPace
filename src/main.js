c03a708a9bf5cb8df3384d98c81a5675595a8dcb
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-14 03:20:52 +0530

    Cleaned and changed some codes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-//const { invoke } = window.__TAURI__.tauri;
-
 import { invoke } from '@tauri-apps/api/tauri'
 const invoke = window.__TAURI__.invoke
 
+
+// CONFIGURING SPLASHSCREEN START ===================================== //
 document.addEventListener('DOMContentLoaded', () => {
   invoke('close_splashscreen')
 })
-
+// ======================================= CONFIGURING SPLASHSCREEN END //
