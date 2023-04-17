9fcb85f3666867973667baf9f1d4ddbd4da5050a
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-04-18 02:05:08 +0530

    Did some formatting

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ const invoke = window.__TAURI__.invoke
 
 // CONFIGURING SPLASHSCREEN START ===================================== //
 document.addEventListener('DOMContentLoaded', () => {
-  invoke('close_splashscreen')
+	invoke('close_splashscreen')
 })
 // ======================================= CONFIGURING SPLASHSCREEN END //
